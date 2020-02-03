8603f6330ceae63272e90a8a7d781e34f40876c2
Kimihisa Ishizaki <ishizaki@asobitagari.xyz>
2020-02-03 18:48:46 +0900

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ManaComponent } from './mana/mana.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ManaComponent
+    ManaComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
